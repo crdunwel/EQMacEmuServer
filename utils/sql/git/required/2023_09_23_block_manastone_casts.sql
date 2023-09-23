028f1aa9a50e25b088ab3a2c086b410083149e84
Clayton Dunwell <clayton.dunwell@gmail.com>
2023-09-23 17:35:39 -0400

    block manastone cast outside classic

---
+++ utils/sql/git/required/2023_09_23_block_manastone_casts.sql
@@ -0,0 +1,15 @@
+INSERT INTO blocked_spells (spellid, type, zoneid, x, y, z, x_diff, y_diff, z_diff, message, description)
+SELECT
+    (SELECT id FROM spells_new WHERE id = (SELECT clickeffect FROM items WHERE name = "Manastone" LIMIT 1) LIMIT 1) AS spell_id,
+    1 AS type,
+    zone.id AS zoneid,
+    0 AS x,
+    0 AS y,
+    0 AS z,
+    0 AS x_diff,
+    0 AS y_diff,
+    0 AS z_diff,
+    'Manastone unable to be used message sent to client here' AS message,
+    'Manastone internal message here' AS description
+FROM zone
+WHERE expansion != 1 OR long_name IN ('Plane of Hate', 'Plane of Sky', 'Plane of Fear');
